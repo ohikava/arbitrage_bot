d85876357221ca6c25aae8575e2c41b683d72baf
IvanneWest <ivannewest@IvanneWest.168.1.12>
2023-12-17 01:49:58 +0300

    added basic spred searcher

--- arbitrage/arbitrage_bot.py
+++ arbitrage/arbitrage_bot.py
@@ -56,6 +56,48 @@ class ArbitrageBot:
 
         with open("all_data.json", 'w') as file:
             json.dump(self.depths, file)
+        
+        print(self.find_spread(self.depths['BTCUSDT']))
+
+    def _find_spread_by_two_cex(self, cex1_info, cex2_info, spread_limit=0.05):
+        bids1 = [float(i[0]) for i in cex1_info['bids']]
+        bids2 = [float(i[0]) for i in cex2_info['bids']]
+
+        asks1 = [float(i[0]) for i in cex1_info['asks']]
+        asks2 = [float(i[0]) for i in cex2_info['asks']]
+
+        spread1 = bids1[0] / asks2[0] - 1
+        if bids1[0] > asks2[0] and spread1 > spread_limit:
+            return ('bids', 'asks')
+        
+        spread2 = bids2[0] / asks1[0] - 1
+        if bids2[0] > asks1[0] and spread2 > spread_limit:
+            return ('asks', 'bids')
+
+        return (None, None)
+
+    def find_spread(self, data):
+        map_ba = {
+        'asks': 'buy',
+        'bids': 'sell'
+        }
+
+        for cex1 in data:
+            for cex2 in data:
+                if cex1 == cex2:
+                    continue 
+                
+                cex1_info = data[cex1]
+                cex2_info = data[cex2]
+                scan = self._find_spread_by_two_cex(cex1_info, cex2_info)
+
+                if scan[0]:
+                    price1 = float(cex1_info[scan[0]][0][0])
+                    price2 = float(cex2_info[scan[1]][0][0])
+
+                    spread = max(price1, price2)/min(price1, price2)-1
+                    print(f"CEX 1: {map_ba[scan[0]]}, CEX 2: {map_ba[scan[1]]}. Price 1: {price1}, Price 2: {price2}. Spread: {spread}%")
+
 
 
     
